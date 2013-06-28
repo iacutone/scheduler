b1ef0316dec231d495d805734f86d37b695bb5a1
Eric Iacutone <eric.iacutone@gmail.com>
2013-06-28 15:20:20 -0400

    more js changes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,11 @@
-$(function(){
-  $(".tablesorter").tablesorter();
-});
+// $(function(){
+//   $(".tablesorter").tablesorter();
+// });
 
 // $('#user-day1-modal').modal('toggle')
 // $('#user-day2-modal').modal('toggle')
 
-<% if @time1 > 1 %>
-$('#user_time1_1130').remove()
+// <% if @time1 > 1 %>
+// $('#user_time1_1130').remove()
 
 
