4eefa69aab930944e222c0ab1f942966db1df57f
Eric Iacutone <eric.iacutone@gmail.com>
2013-06-28 14:37:55 -0400

    changed made to time slots values

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,5 +2,10 @@ $(function(){
   $(".tablesorter").tablesorter();
 });
 
-$('#user-day1-modal').modal('toggle')
-$('#user-day2-modal').modal('toggle')
+// $('#user-day1-modal').modal('toggle')
+// $('#user-day2-modal').modal('toggle')
+
+<% if @time1 > 1 %>
+$('#user_time1_1130').remove()
+
+
