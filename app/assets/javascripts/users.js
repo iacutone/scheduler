69f3c861ed53e3ddf41fac5e2ce7c1c4a019e6b8
Eric Iacutone <eric.iacutone@gmail.com>
2013-07-04 00:03:10 -0400

    changes to userjs so jquery works for all variables

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,154 +10,154 @@ if (gon.time2 > 4) {
 	$('.wrapper2').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time3 > 4) {
 	$('.wrapper3').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time4 > 4) {
 	$('.wrapper4').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time5 > 4) {
 	$('.wrapper5').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time6 > 4) {
 	$('.wrapper6').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time7 > 4) {
 	$('.wrapper7').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time8 > 4) {
 	$('.wrapper8').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time9 > 4) {
 	$('.wrapper9').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time10 > 4) {
 	$('.wrapper10').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time11 > 4) {
 	$('.wrapper11').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time12 > 4) {
 	$('.wrapper12').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time13 > 4) {
 	$('.wrapper13').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time14 > 4) {
 	$('.wrapper14').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time15 > 4) {
 	$('.wrapper15').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time16 > 4) {
 	$('.wrapper16').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time17 > 4) {
 	$('.wrapper17').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time18 > 4) {
 	$('.wrapper18').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time19 > 4) {
 	$('.wrapper19').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time20 > 4) {
 	$('.wrapper20').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time21 > 4) {
 	$('.wrapper21').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time22 > 4) {
 	$('.wrapper22').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time23 > 4) {
 	$('.wrapper23').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time24 > 4) {
 	$('.wrapper24').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time25 > 4) {
 	$('.wrapper25').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time26 > 4) {
 	$('.wrapper26').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time27 > 4) {
 	$('.wrapper27').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time28 > 4) {
 	$('.wrapper28').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time29 > 4) {
 	$('.wrapper29').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time30 > 4) {
 	$('.wrapper30').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time31 > 4) {
 	$('.wrapper31').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time32 > 4) {
 	$('.wrapper32').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time33 > 4) {
 	$('.wrapper33').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time34 > 4) {
 	$('.wrapper34').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time35 > 4) {
 	$('.wrapper35').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time36 > 4) {
 	$('.wrapper36').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time37 > 4) {
 	$('.wrapper37').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time38 > 4) {
 	$('.wrapper38').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time39 > 4) {
 	$('.wrapper39').remove();
 }
 
-if (gon.time2 > 4) {
+if (gon.time40 > 4) {
 	$('.wrapper40').remove();
 }
